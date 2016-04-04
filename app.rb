94963a56297b052ac406e232fccfa9099df5e0b1
adi-botak- <adityautamawijaya@gmail.com>
2016-04-04 09:35:02 +0800

    added skeleton to url shortner app

--- app.rb
+++ app.rb
@@ -1,13 +1,31 @@
 require 'sinatra'
 require 'json'
 require 'base64'
-require_relative 'db/url'
+require_relative 'models/url'
 
-# Url Shortner web application
+# url shortner web application
 class UrlShortnerAPI < Sinatra::Base
-  
-  get '/?' do
-    'Url Shortner is ready to crunch some Urls'
-  end
+	before do 
+		ShortUrl.setup
+	end
 
+	get '/?' do
+		'Hello, World!'
+	end
+
+	get '/api/v1/urls/?' do
+		content_type 'application/json'
+		id_list = ShortUrl.all
+		{ url_id: id_list }.to_json
+	end
+
+	get '/api/v1/urls/:id.json' do
+		content_type 'application/json'
+		begin
+			{ url: ShortUrl.find(params[:id]) }.to_json
+		rescue => e
+			status 404
+			logger.info "FAILED to GET url: #{e.inspect}"
+		end
+	end
 end
---
+++ models/url.rb
@@ -0,0 +1,40 @@
+require 'json'
+
+# properties of a short url
+class ShortUrl
+	STORE_DIR = 'db/'.freeze
+
+	attr_accessor :id, :url, :title, :description, :public
+
+	def initialize(new_url)
+		@id = new_url['id'] # id of a url
+		@link = new_url['link'] # url link
+		@title = new_url['title'] # title of url
+		@description = new_url['description'] # additional information for url
+		@public = new_url['public'] # url privacy
+	end
+
+	def self.setup
+		Dir.mkdir(ShortUrl::STORE_DIR) unless Dir.exist? STORE_DIR
+	end
+
+	def self.all
+		Dir.glob(STORE_DIR + '*.txt').map do |filename|
+			filename.match(%r{db\/(.*)\.txt})[1]
+		end
+	end
+
+	def to_json(options = {})
+		JSON({ id: @id,
+			   link: @link,
+			   title: @title,
+			   description: @description,
+			   public: @public },
+			  options)
+	end
+
+	def self.find(find_id)
+		url_file = File.read(STORE_DIR + find_id + '.txt')
+		ShortUrl.new JSON.parse(url_file)
+	end
+end
