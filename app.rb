637ab34cced843a1165d1fa528859733f5377665
adi-botak- <adityautamawijaya@gmail.com>
2016-04-04 10:27:23 +0800

    modified the spaces

--- app.rb
+++ app.rb
@@ -5,27 +5,27 @@ require_relative 'models/url'
 
 # url shortner web application
 class UrlShortnerAPI < Sinatra::Base
-	before do
-		ShortUrl.setup
-	end
+  before do
+    ShortUrl.setup
+  end
 
-	get '/?' do
-		'Hello, World!'
-	end
+  get '/?' do
+    'Hello, World!'
+  end
 
-	get '/api/v1/urls/?' do
-		content_type 'application/json'
-		id_list = ShortUrl.all
-		{ url_id: id_list }.to_json
-	end
+  get '/api/v1/urls/?' do
+    content_type 'application/json'
+    id_list = ShortUrl.all
+    { url_id: id_list }.to_json
+  end
 
-	get '/api/v1/urls/:id.json' do
-		content_type 'application/json'
-		begin
-			{ url: ShortUrl.find(params[:id]) }.to_json
-		rescue => e
-			status 404
-			logger.info "FAILED to GET url: #{e.inspect}"
-		end
-	end
+  get '/api/v1/urls/:id.json' do
+    content_type 'application/json'
+    begin
+      { url: ShortUrl.find(params[:id]) }.to_json
+    rescue => e
+      status 404
+      logger.info "FAILED to GET url: #{e.inspect}"
+    end
+  end
 end
