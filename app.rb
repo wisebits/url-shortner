299500cf0fb13243adc7cacaa76d96fc39157b74
nweat <nikkiweat@gmail.com>
2016-04-06 15:58:32 +0800

    Added POST route

--- app.rb
+++ app.rb
@@ -10,7 +10,9 @@ class UrlShortnerAPI < Sinatra::Base
   end
 
   get '/?' do
-    'Hello, World!'
+    '<a href= "https://github.com/wisebits/url-shortner" target="new_blank">
+    URL shortener API service
+    </a>'
   end
 
   get '/api/v1/urls/?' do
@@ -22,10 +24,29 @@ class UrlShortnerAPI < Sinatra::Base
   get '/api/v1/urls/:id.json' do
     content_type 'application/json'
     begin
-      { url: ShortUrl.find(params[:id]) }.to_json
+      { url_details: ShortUrl.find(params[:id]) }.to_json
     rescue => e
       status 404
       logger.info "FAILED to GET url: #{e.inspect}"
+    end
+  end
+
+  post '/api/v1/urls/?' do
+    content_type 'application/json'
+
+    begin
+      new_data = JSON.parse(request.body.read)
+      new_url_data = ShortUrl.new(new_data)
+      if new_url_data.save
+        logger.info "NEW URL STORED: #{new_url_data.id}"
+      else
+        halt 400, "Could not store url details: #{new_url_data}"
+      end
+
+      redirect 'api/v1/urls/' + new_url_data.id + '.json'
+    rescue => e
+      status 400
+      logger.info "Failed to create new url: #{e}"
     end
   end
 end
