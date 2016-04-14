be6563e9c12f9cc3e8a6b942230d2e778249d687
adi-botak- <adityautamawijaya@gmail.com>
2016-04-14 23:41:56 +0800

    Modified the spaces and some requirements

--- db/migrations/002_users_create.rb
+++ db/migrations/002_users_create.rb
@@ -1,15 +1,14 @@
 require 'sequel'
 
 Sequel.migration do	
-	change do
-		create_table(:users) do
-			primary_key :id
-
-			String :email, unique: true, null: false
-			String :password, unique: true, null: false
-			String :account_status, unique: true, null: false
-			DateTime :created_at
-			DateTime :updated_at
-		end
-	end
+  change do
+    create_table(:users) do
+      primary_key	:id
+      String	:email, unique: true, null: false
+      String	:password, unique: true, null: false
+      String	:account_status, null: false
+      DateTime	:created_at
+      DateTime	:updated_at
+    end
+  end
 end
