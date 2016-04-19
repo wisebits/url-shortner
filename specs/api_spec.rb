8423e0e678890437d822bcec97869000a3f75e06
adi-botak- <adityautamawijaya@gmail.com>
2016-04-19 22:16:13 +0800

    Fixed some typos

--- specs/api_spec.rb
+++ specs/api_spec.rb
@@ -19,15 +19,15 @@ describe 'URL resource calls' do
   describe 'Creating new URLs' do
     it 'HAPPY: should create a new unique URL' do
       req_header = { 'CONTENT_TYPE' => 'application/json' }
-      req_body = { title: "test", full_url: "http://test.com", short_url: "http://wisebits/bfdd" }.to_json
-      post 'api/v1/urls/', req_body, req_header
+      req_body = { title: 'test', full_url: 'http://test.com', short_url: 'http://wisebits/bfdd' }.to_json
+      post '/api/v1/urls/', req_body, req_header
       _(last_response.status).must_equal 201
       _(last_response.location).must_match(%r{http://})
     end
 
     it 'SAD: should not create URLs with duplicate names' do
     	req_header = { 'CONTENT_TYPE' => 'application/json' }
-    	req_body = { title: "test", full_url: 'http://test.com', short_url: 'http://wisebits/bfdd' }.to_json
+    	req_body = { title: 'test', full_url: 'http://test.com', short_url: 'http://wisebits/bfdd' }.to_json
     	post '/api/v1/urls/', req_body, req_header
     	post '/api/v1/urls/', req_body, req_header
     	_(last_response.status).must_equal 400
@@ -37,7 +37,7 @@ describe 'URL resource calls' do
 
   describe 'Finding existing URLs' do
     it 'HAPPY: should find an existing URL' do
-    	new_url = Url.create(title: "test23", full_url: 'http://test23.com', short_url: 'http://wisebits/bfdd23')
+    	new_url = Url.create(title: 'test23', full_url: 'http://test23.com', short_url: 'http://wisebits/bfdd23')
     	get "/api/v1/urls/#{new_url.id}"
     	_(last_response.status).must_equal 200
 
@@ -51,10 +51,10 @@ describe 'URL resource calls' do
     end
   end
 
-  describe 'Getting an index of existing urls' do
-    it 'HAPPY: should find list of existing urls' do
+  describe 'Getting an index of existing URLs' do
+    it 'HAPPY: should find list of existing URLs' do
       (1..5).each { |i| Url.create(title: "Project #{i}" ,full_url: "http://test#{i}.com", short_url: "http://wisebits/bfdd#{i}") }
-      result = get '/api/v1/urls'
+      result = get '/api/v1/urls/'
       urls = JSON.parse(result.body)
       _(urls['data'].count).must_equal 5
     end
