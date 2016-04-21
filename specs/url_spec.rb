75ce8429f2f44fd87087884c553bba40b5dca6a1
adi-botak- <adityautamawijaya@gmail.com>
2016-04-21 12:43:35 +0800

    Removed the test for root route

--- specs/url_spec.rb
+++ specs/url_spec.rb
@@ -1,13 +1,5 @@
 require_relative './spec_helper'
 
-describe 'Test root route' do 
-  it 'should find the root route' do
-    get '/'
-    last_response.body.must_include 'URL Shortner'
-    last_response.status.must_equal 200
-  end
-end
-
 describe 'URL resource calls' do
   before do
     Permission.dataset.delete
