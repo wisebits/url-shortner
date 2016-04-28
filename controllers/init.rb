ea0e7668709396103dce0b912b9b7b8a8e4266dc
omarsar <ellfae@gmail.com>
2016-04-28 21:08:01 +0800

    separated permission and urls controllers

--- controllers/init.rb
+++ controllers/init.rb
@@ -1,3 +1,4 @@
 require_relative 'base'
 require_relative 'urls_controller'
 require_relative 'users_controller'
+require_relative 'permissions_controller'
--- controllers/urls_controller.rb
+++ controllers/urls_controller.rb
@@ -21,7 +21,7 @@ class UrlShortnerAPI < Sinatra::Base
 
     id = params[:id]
     url = Url[id]
-    permissions = url ? Url[id].permissions : []
+    permissions = url ? Url[id].users : []
 
     if url
       JSON.pretty_generate(data: url, relationships: permissions)
@@ -45,46 +45,6 @@ class UrlShortnerAPI < Sinatra::Base
     new_location = URI.join(@request_url.to_s + '/', saved_url.id.to_s).to_s
     status 201
 
-    headers('Location' => new_location)
-  end
-
-  get '/api/v1/urls/:id/permissions/?' do
-    content_type 'application/json'
-
-    url = Url[params[:id]]
-
-    JSON.pretty_generate(data: url.permissions)
-  end
-
-  get '/api/v1/urls/:url_id/permissions/:id/?' do
-    content_type 'application/json'
-
-    begin
-      puts params[:id]
-      #doc_url = URI.join(@request_url.to_s + '/', 'document')
-      permission = Permission.where(url_id: params[:url_id], id: params[:id]).first
-      halt(404, 'Permission not found') unless permission
-      JSON.pretty_generate(data: {
-        permission: permission     
-        })
-    rescue => e
-      status 400
-      logger.info "FAILED to process GET `d request: #{e.inspect}"
-      e.inspect
-    end
-  end
-
-  post '/api/v1/urls/:url_id/permissions/?' do
-    begin
-      new_data = JSON.parse(request.body.read)
-      url = Url[params[:url_id]]
-      saved_permission = url.add_permission(new_data)
-    rescue => e
-      logger.info "FAILED to create new permission: #{e.inspect}"
-      halt 400
-    end
-    status 201
-    new_location = URI.join(@request_url.to_s + '/', saved_permission.id.to_s).to_s
     headers('Location' => new_location)
   end
 end
